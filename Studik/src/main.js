f9940d3dd9e0ecebd85d48237ed272c61508150a
BNazar002 <62611772+BNazar002@users.noreply.github.com>
2022-05-26 12:55:51 +0300

    final beta
    -animations

--- Studik/src/main.js
+++ Studik/src/main.js
@@ -14,3 +14,35 @@ window.addEventListener('scroll', function() {
         'top': 365 + (scrollTop / 30) + "vh"
     })
 });
+
+
+
+var imgs = [
+    "images/screens/phone-1_night.png",
+    "images/screens/phone-1_day.png",
+    "images/screens/phone-2_night.png",
+    "images/screens/phone-2_day.png",
+    "images/screens/phone-3_night.png",
+    "images/screens/phone-3_day.png",
+];
+var n = 0;
+time = 1000;
+play = setInterval("chgImg(0)", 7000);
+
+
+// while (true) {
+//     chgImg(0)
+// }
+
+function chgImg(number) {
+    // if (number != 0) n = number;
+    $('#slide_show').fadeOut(time, function() { //для картинок
+        $(this).attr('src', imgs[n]).fadeIn(time);
+    });
+    // $('#slide_show').attr('src', imgs[n]).fadeIn(time);
+    n++;
+    // alert(n)
+    if (n >= imgs.length) n = 0;
+}
+
+new WOW().init();
